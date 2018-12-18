3734032fc4c1c8fbdafeee1de6c2e739dd27efa0
REÇ <ecoban@sutas.com.tr>
2018-12-18 11:44:28 +0300

    first template

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+import InclinedCard from '../modules/inclinedCard/inclined-card';
+
+const App = () => (
+  <Fragment>
+    <InclinedCard/>
+  </Fragment>
+)
+
+export default App
---
+++ src/modules/inclinedCard/inclined-card.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './inclinedCard.scss';
+
+const inclinedCard = () => {
+    return (
+        <section className="section-book">
+            <div className="row">
+                <div className="book">
+                
+                </div>  
+            </div>
+        </section>
+    );
+}
+
+export default inclinedCard
